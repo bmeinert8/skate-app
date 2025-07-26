ef563ef62c2f616fce213eca6e551bfbcbf58301
Brian Meinert <brian.meinert1@gmail.com>
2025-07-25 23:42:16 -0400

    added ablilty to collapse and expand video section detail. update selection menu to display the different list options for media selections

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
   const selectionMenu = document.querySelector('.js-selection');
+  const moreButton = document.querySelector('.js-elipsis');
+  const videoDescription = document.querySelector('.js-video-description');
+  const collapseButton = document.querySelector('.js-collapse');
 
   // Toggle the navigation menu when the hamburger icon is clicked
   hamburger.addEventListener('click', () => {
@@ -33,6 +36,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Add 'selected' class to clicked link
       link.classList.add('selected');
+
+      // Get the href attribute (e.g., "#new") and remove the "#" to match the list ID
+      const targetId = link.getAttribute('href').substring(1);
+
+      // Get all lists
+      const lists = document.querySelectorAll(
+        'ul[id="new"], ul[id="popular"], ul[id="trending"]'
+      );
+
+      // Hide all lists and show the target list
+      lists.forEach((list) => {
+        if (list.id === targetId) {
+          list.classList.remove('list-hidden');
+        } else {
+          list.classList.add('list-hidden');
+        }
+      });
     });
+  });
+
+  // Toggle video description visibility when the more icon is clicked
+  moreButton.addEventListener('click', () => {
+    videoDescription.classList.toggle('hidden');
+    collapseButton.classList.remove('hidden');
+    moreButton.classList.toggle('hidden');
+  });
+
+  // Collapse video description when the collapse icon is clicked
+  collapseButton.addEventListener('click', () => {
+    videoDescription.classList.add('hidden');
+    collapseButton.classList.add('hidden');
+    moreButton.classList.remove('hidden');
   });
 });
